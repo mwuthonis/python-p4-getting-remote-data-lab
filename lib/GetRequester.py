9976f112d7904e8fbf4645c58cfd0b9080db56e6
Shelmith Muthoni <muthonishelmith136@gmail.com>
2025-05-20 16:57:35 +0300

    Getting remote data

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,20 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        try:
+            return json.loads(response_body)
+        except json.JSONDecodeError:
+            print("Error decoding JSON")
+            return None
+
+if __name__ == "__main__":
+    url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+    get_requester = GetRequester(url)
+
+    print("Raw Response Body:\n", get_requester.get_response_body())
+    print("\nParsed JSON:\n", get_requester.load_json())
